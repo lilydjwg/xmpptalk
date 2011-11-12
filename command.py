75527eedeebcc461901e17ab0c76cb5602d1f7cf
Vayn a.k.a. VT <vayn@vayn.de>
2011-11-13 07:28:43 +0800

    命令处理模块
    Better than nothing.

---
+++ command.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8:
+# @Name: command.py
+import shlex
+
+
+class CommandMixinMixin:
+  def unknown(self, message):
+    pass
+
+  def handle(self, message):
+    lexer = shlex.shlex(message)
+    command = lexer.get_token()
+    body = lexer.instream.read().strip()
+    try:
+      meth = getattr(self, command, None)
+      meth(body)
+    except TypeError:
+      self.unknown(command)
+
+
+class UserCommandMixin(CommandMixinMixin):
+  pass
+
+
+class ManagerCommandMixin(CommandMixinMixin):
+  pass
+
+
+class SysCommandMixin(CommandMixinMixin):
+  pass
