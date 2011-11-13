1973dbc7bf55203721d8249b21bb65523ac1d8f1
Vayn a.k.a. VT <vayn@vayn.de>
2011-11-13 17:26:15 +0800

    完善用户命令框架

--- command.py
+++ command.py
@@ -4,7 +4,7 @@
 import shlex
 
 
-class CommandMixinMixin:
+class BaseHandler:
   def unknown(self, message):
     pass
 
@@ -18,14 +18,67 @@ class CommandMixinMixin:
     except TypeError:
       self.unknown(command)
 
+  def do_help(self):
+    pass
 
-class UserCommandMixin(CommandMixinMixin):
+
+class UserInfo:
+  def do_history(self):
+    """Chatting history"""
+    pass
+
+  def do_online(self):
+    """The number of online people"""
+    pass
+
+  def do_Listing(self):
+    """Listing all users in the group"""
+    pass
+
+  def do_admin(self):
+    """Listing all administrators"""
+    pass
+
+  def do_seek(self, body):
+    """Get one's information"""
+    pass
+
+  def do_stats(self):
+    """Listing statistics"""
+    pass
+
+
+class UserSetting:
+  def do_opendm(self, body):
+    """Accepting direct message or not"""
+    pass
+
+  def do_prevent(self, body):
+    """The resources you don't want to receive"""
+    pass
+
+
+class UserCommandMixin(BaseHandler, UserInfo, UserSetting):
+  def do_nick(self, body):
+    """Changing user's nick"""
+    pass
+
+  def do_dm(self, body):
+    """Direct message"""
+    pass
+
+  def do_snooze(self, body):
+    """Suspending the account"""
+    pass
+
+  def do_quit(self):
+    """Quiting group"""
+    pass
+
+
+class ManagerCommandMixin(BaseHandler):
   pass
 
 
-class ManagerCommandMixin(CommandMixinMixin):
-  pass
-
-
-class SysCommandMixin(CommandMixinMixin):
+class SysCommandMixin(BaseHandler):
   pass
