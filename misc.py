3ffb5ea5b9e52596855fa9d822559685e0369e49
lilydjwg <lilydjwg@gmail.com>
2011-12-03 00:48:15 +0800

    fix jid validation

--- misc.py
+++ misc.py
@@ -30,7 +30,7 @@ PERM_SYSADMIN = 4
 
 NOW = datetime.datetime.utcnow
 
-re_jid = re.compile(r'[^@ ]+@(?:\w+\.)+\w{2,4}')
+re_jid = re.compile(r'[^@ ]+@(?:[\w-]+\.)+\w{2,4}')
 dateformat = _('%m-%d %H:%M:%S')
 timeformat = _('%H:%M:%S')
 logger = logging.getLogger(__name__)
