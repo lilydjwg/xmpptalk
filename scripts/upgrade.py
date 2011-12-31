e89838118f7822737e72a24ea9855e0449ff3961
lilydjwg <lilydjwg@gmail.com>
2011-12-31 17:02:26 +0800

    another import path fix for upgrade script

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8
 
 import sys
-sys.path.insert(0, '..')
+sys.path.insert(0, '.')
 from models import connection, User
 
 col = connection[User.__database__][User.__collection__]
