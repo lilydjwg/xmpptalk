c7cca1d0b2235624902d6671e5090325bb0a12aa
lilydjwg <lilydjwg@gmail.com>
2012-03-01 20:35:16 +0800

    better words

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ class ChatBot(MessageMixin, UserMixin, EventHandler, XMPPFeatureHandler):
 
     if config.warnv105 and jid.resource.startswith('Talk.v105'):
       # No need to translate; GTalk only has a v105 for Chinese.
-      self.send_message(jid, '你正在使用非加密版的 GTalk v105。网络上的其它人可能会截获您的消息。请不要这样！')
+      self.send_message(jid, '警告：你正在使用非加密版的 GTalk v105。网络上的其它人可能会截获您的消息。请不要这样！')
 
     return True
 
